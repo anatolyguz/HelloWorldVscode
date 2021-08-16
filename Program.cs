7fb8437cf3f6c5f6f09b75e41de9aa5e282d2199
anatolyguz <anatolyguz@gmail.com>
2021-08-16 17:29:57 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -9,6 +9,8 @@ namespace HelloWorldVscode
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World second!");
             Console.WriteLine("Hello World 3!");
+            Console.WriteLine("Hello World 4!");
+            
         }
     }
 }
